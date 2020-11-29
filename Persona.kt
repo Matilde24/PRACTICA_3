ef214b38afb200e80693530277a5a4b6445cadc8
Matilde24 <matyarostegui@gmail.com>
2020-11-29 14:22:00 -0600

    ejercicios

--- Persona.kt
+++ Persona.kt
@@ -15,7 +15,7 @@ class Persona(var nombre:String, var apellido:String)
 
 fun main(){
     var persona = Persona("Yesser","Miranda")
-    // var nueva = Persona()
+     var nueva = Persona()
     // nueva.nombre = Persona("Juan")
     // nueva.apellido = "Lopez"
 
